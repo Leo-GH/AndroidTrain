45f483d0cfe672fa7008c34b5ce905a7f68a4816
Leo <2423493653@qq.com>
2019-12-26 16:00:29 +0800

    BMI制作

--- BMI163/app/src/main/java/com/leo/bmi163/MainActivity.java
+++ BMI163/app/src/main/java/com/leo/bmi163/MainActivity.java
@@ -3,12 +3,49 @@ package com.leo.bmi163;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText weight;
+    EditText height;
+    Button submit;
+    TextView bmi;
+    RadioGroup sex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        submit.setOnClickListener(this);
+    }
+
+    private void initView() {
+        sex = findViewById(R.id.radio_group);
+        weight = findViewById(R.id.et_weight);
+        height = findViewById(R.id.et_height);
+        submit = findViewById(R.id.submit);
+        bmi = findViewById(R.id.bmi_text);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.submit:
+                weight.getText();
+                height.getText();
+                sex.getAutofillValue();
+                bmi.setText("");
+                break;
+            default:
+        }
+
     }
 }
